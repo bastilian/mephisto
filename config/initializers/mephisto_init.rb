737efd5c69d26e7f6542af740317c783e081948a
Sebastian Gräßl <sebastian@validcode.me>
2012-11-10 13:12:57 +0100

    register liquid tags without cleanup_application

--- config/initializers/mephisto_init.rb
+++ config/initializers/mephisto_init.rb
@@ -16,14 +16,6 @@ class ActionController::Dispatcher
     Mephisto.liquid_filters.each { |mod| Liquid::Template.register_filter mod }
     Mephisto.liquid_tags.each { |name, klass| Liquid::Template.register_tag name, klass }
   end
-  
-  def cleanup_application_with_plugins
-    returning cleanup_application_without_plugins do
-      self.class.register_liquid_tags
-    end
-  end
-  
-  alias_method_chain :cleanup_application, :plugins
 end
 
 ActionController::Dispatcher.register_liquid_tags
