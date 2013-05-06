aebe19d604c1266d61ea2fd1ceea4ee3e18bac3b
Sebastian Gräßl <sebastian@validcode.me>
2013-05-06 20:24:37 +0200

    Site.create automatically creates a Home-section (fixes #1)

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,4 @@
             :current_theme_path => "simpla"
 )
 
-@site.sections.create(:name => "Home", :path => '', :articles_per_page => 15, :template => 'home.liquid', :archive_path => 'archives')
 @site.members.create(:login => 'admin', :email => "admin@changeme.com", :password => "testpassword", :password_confirmation => "testpassword", :admin => true)
