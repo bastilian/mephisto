e3d10ec12783dc77bc423310ffa1fb989e32d2bf
Sebastian Gräßl <sebastian@validcode.me>
2013-05-30 23:10:05 +0200

    let's get rid of the xmlrpc_patch

--- config/initializers/mephisto_init.rb
+++ config/initializers/mephisto_init.rb
@@ -1,7 +1,6 @@
 # this is for standard library loading and configuration.  All the hardcore monkey patching is in the mephisto plugin.
 require 'action_controller/dispatcher'
 require 'rubypants'
-require 'xmlrpc_patch'
 require 'base64'
 
 # temporarily moved to vendor/plugins/aaa/init.rb to make sure
